2fa2979c78ad4e84a2ffbe114f9d71873c3caae0
Zuz <zk439532@students.mimuw.edu.pl>
2025-05-04 22:24:39 +0200

    Add base models to the app.

--- django_project/routes_editor/routes/models.py
+++ django_project/routes_editor/routes/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class BackgroundImage(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='backgrounds/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Route(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    background = models.ForeignKey(BackgroundImage, on_delete=models.CASCADE)
+    # Each Route is connected to exactly one User and one BackgroundImage
+    
+    title = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} (by {self.user.username})"
+
+class Point(models.Model):
+    route = models.ForeignKey(Route, on_delete=models.CASCADE, related_name='points')
+    # Each points configuration is assigned to a certain Route
+    x = models.IntegerField()
+    y = models.IntegerField()
+    order = models.IntegerField()
+
+    class Meta:
+        ordering = ['order']
+
+    def __str__(self):
+        return f"Point ({self.x}, {self.y}) in {self.route.title}"
