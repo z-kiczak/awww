7cf503ac4debe7511f925bee2958919dc897f82b
Zuz <zk439532@students.mimuw.edu.pl>
2025-05-08 18:54:07 +0200

    Finish the basic functionalities.

--- django_project/routes_editor/routes/urls.py
+++ django_project/routes_editor/routes/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('routes/', views.route_list, name='route_list'),
-    path('routes/create/', views.route_create, name='route_create'),
-    path('routes/<int:pk>/', views.route_detail, name='route_detail'),
-    path('routes/<int:pk>/add_point/', views.add_point, name='add_point'),
-    path('routes/<int:route_pk>/delete_point/<int:point_pk>/', views.delete_point, name='delete_point'),
+    path('route_list', views.route_list, name='route_list'),
+    path('create/', views.route_create, name='route_create'),
+    path('<int:pk>/', views.route_detail, name='route_detail'),
+    path('<int:pk>/add_point/', views.add_point, name='add_point'),
+    path('<int:route_pk>/delete_point/<int:point_pk>/', views.delete_point, name='delete_point'),
 ]
--- django_project/routes_editor/routes_editor/urls.py
+++ django_project/routes_editor/routes_editor/urls.py
@@ -15,8 +15,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from routes import views as routes_views
+from django.contrib.auth import views as auth_views
+    
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include([
+        path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+        path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+        path('register/', routes_views.register, name='register'),
+    ])),
+    path('routes/', include('routes.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
